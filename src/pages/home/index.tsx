1b7b4de32f6c8297432fecb4aefb966d11323f66
Lavinia Cirilo Jeronimo <FRI0026031@aluno.firjansenaisesi.com.br>
2023-02-06 21:33:18 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home(){
+    return (
+        <div>
+            <h1>POKEMONS</h1>
+            <ul>
+                <li>
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="Pokemon"></img>
+        
+                </li>
+            </ul>
+        </div> 
+    );
+}
